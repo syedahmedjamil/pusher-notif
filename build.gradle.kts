2099b39e50d01eb13e86b004c2bf8bf1f59e3b58
Syed Ahmed Jamil - Local <ahmedjamilcs@gmail.com>
2023-12-20 23:04:42 +0500

    adeed unit test report aggregation

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,10 +1,36 @@
+import org.gradle.internal.impldep.bsh.commands.dir
+
 // Top-level build file where you can add configuration options common to all sub-projects/modules.
 plugins {
     alias(libs.plugins.androidApplication) apply false
     alias(libs.plugins.jetbrainsKotlinAndroid) apply false
     alias(libs.plugins.kotlinKapt) apply false
     alias(libs.plugins.org.jetbrains.kotlin.jvm) apply false
+    jacoco
+    java
+}
+//apply jacoco
+apply(plugin = "jacoco")
+apply(plugin = "android-reporting")
+
+jacoco {
+    toolVersion = "0.8.9"
 }
 
-apply(plugin = "android-reporting")
+val testReport = tasks.register("testReport", TestReport::class) {
+    outputs.upToDateWhen { false }
+    destinationDir = file("$buildDir/reports/unitTests")
+    subprojects {
+        reportOn(project.tasks.withType<Test>().map { it.binaryResultsDirectory })
+    }
+}
+
+tasks.withType<Test> {
+    useJUnitPlatform()
+    finalizedBy(testReport)
+    ignoreFailures = true
+    testLogging {
+        events("passed", "skipped", "failed")
+    }
+}
 
